92ee772021ea06c07bbb242d51f908bbfa243c53
richcire <eric9907@naver.com>
2022-08-04 17:02:15 +0900

    add navigation bar

--- public/components/NavBar.tsx
+++ public/components/NavBar.tsx
@@ -1,10 +1,24 @@
 import styled from "styled-components";
 
-const Container = styled.div`
+const Bar = styled.div`
   width: 100%;
-  background-color: aqua;
+  background-color: #c6cacf;
+  height: 100px;
+`;
+
+const NavContainer = styled.div`
+  margin: 0 auto;
+  display: flex;
+  height: 100%;
+  width: 100%;
+  max-width: 1024px;
+  background-color: whitesmoke;
 `;
 
 export default function NavBar() {
-  return <Container>this is navigation bar</Container>;
+  return (
+    <Bar>
+      <NavContainer></NavContainer>
+    </Bar>
+  );
 }
