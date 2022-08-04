ee4a0f017fa90862c3f1bd5420b5ad51bad66f31
richcire <eric9907@naver.com>
2022-08-04 16:51:53 +0900

    add styled components

---
+++ public/components/NavBar.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  background-color: aqua;
+`;
+
+export default function NavBar() {
+  return <Container>this is navigation bar</Container>;
+}
